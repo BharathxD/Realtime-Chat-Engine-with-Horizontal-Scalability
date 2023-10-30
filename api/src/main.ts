22baae19b0a9fb77081ed460a099680e52771a0d
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-10-30 17:11:15 +0530

    feat: add documentation and refactor the code

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,45 +1,60 @@
-import fastifyCors from "@fastify/cors";
-import { config } from "dotenv";
-import fastify from "fastify";
-import fastifySocketIO from "fastify-socket.io";
+import fastify, { FastifyInstance } from 'fastify';
+import fastifySocketIO from 'fastify-socket.io';
+import { config } from 'dotenv';
+import fastifyCors from '@fastify/cors';
 
-config()
+config();
 
-const PORT = +(process.env.PORT || '3001');
-const HOST = process.env.HOST || '0.0.0.0';
-const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3000";
-const REDIS_URL = process.env.REDIS_URL;
+const PORT: number = +(process.env.PORT || '3001');
+const HOST: string = process.env.HOST || '0.0.0.0';
+const CORS_ORIGIN: string = process.env.CORS_ORIGIN || 'http://localhost:3000';
+const REDIS_URL: string | undefined = process.env.REDIS_URL;
 
 if (!REDIS_URL) {
-    console.error("Missing REDIS_URL");
+    console.error('Missing REDIS_URL');
     process.exit(1);
 }
 
-const buildServer = async () => {
-    const app = fastify();
-    await app.register(fastifyCors, {
-        origin: CORS_ORIGIN
-    })
-    await app.register(fastifySocketIO)
-    app.get("/health-check", (_, reply) => {
-        return reply.status(200).send({
-            status: "OK",
-            port: PORT
-        });
-    })
-    return app;
-}
+/**
+ * Creates and configures the Fastify server.
+ */
+const createFastifyServer = async () => {
+    const server = fastify();
 
-const main = async () => {
-    const app = await buildServer();
+    await server.register(fastifyCors, { origin: CORS_ORIGIN });
+    await server.register(fastifySocketIO);
+
+    server.get('/health-check', (_, reply) => {
+        return reply.status(200).send({
+            status: 'OK',
+            port: PORT,
+        });
+    });
+
+    return server;
+};
+
+
+/**
+ * Start the Fastify server and listen on the specified port and host.
+ */
+const startServer = async () => {
+    const server = await createFastifyServer();
 
     try {
-        await app.listen({ port: PORT, host: HOST });
+        await server.listen({ port: PORT, host: HOST });
         console.log(`Server started at: http://${HOST}:${PORT}`);
-    } catch (error: unknown) {
+    } catch (error) {
         console.error(error);
         process.exit(1);
     }
-}
+};
+
+/**
+ * The main entry point of the application.
+ */
+const main = async () => {
+    await startServer();
+};
 
 main();
