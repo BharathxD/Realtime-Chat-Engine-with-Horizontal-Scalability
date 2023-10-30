bcef5a6f05c30d4709cd12cdcfbe9b17b9a58958
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-10-30 18:19:41 +0530

    feat: configure redis publisher on ws connection

---
+++ api/src/config/redis.ts
@@ -0,0 +1,37 @@
+import Redis from 'ioredis';
+
+const REDIS_URL: string | undefined = process.env.REDIS_URL;
+
+if (!REDIS_URL) {
+    console.error('Missing REDIS_URL');
+    process.exit(1);
+}
+
+/**
+ * A redis publisher
+ * ----
+ * Set it `rejectUnauthorized` to `true`, if you want to encrypt the data
+ * const publisher = new Redis(REDIS_URL, {
+ *  tls: {
+ *    rejectUnauthorized: true
+ *  }
+ * }
+ */
+const publisher = new Redis(REDIS_URL);
+
+/**
+ * A redis subscriber
+ * ----
+ * Set it `rejectUnauthorized` to `true`, if you want to encrypt the data
+ * const subscriber = new Redis(REDIS_URL, {
+ *  tls: {
+ *    rejectUnauthorized: true
+ *  }
+ * }
+ */
+const subscriber = new Redis(REDIS_URL);
+
+export {
+    publisher,
+    subscriber
+};
--- api/src/utils/createFastifyApp.ts
+++ api/src/utils/createFastifyApp.ts
@@ -2,7 +2,8 @@ import fastify from "fastify";
 import fastifyCors from '@fastify/cors';
 import fastifyIO from "fastify-socket.io";
 
-import { CORS_ORIGIN, PORT } from '../config';
+import { CONNECTION_COUND_UPDATED_CHANNEL, CONNECTION_COUNT_KEY, CORS_ORIGIN, PORT } from '../config';
+import { publisher } from "../config/redis";
 
 /**
  * Creates and configures the Fastify app.
@@ -11,10 +12,19 @@ const createFastifyApp = async () => {
     const app = fastify();
     await app.register(fastifyCors, { origin: CORS_ORIGIN });
     await app.register(fastifyIO);
-    app.server.on('connection', (io) => {
+    let currentCount = await publisher.get(CONNECTION_COUNT_KEY);
+    if (!currentCount) {
+        // If there is no connection count set, it should be initialized with 0
+        await publisher.set(CONNECTION_COUNT_KEY, 0);
+    }
+    app.server.on('connection', async (io) => {
         console.info("Client connected.");
-        io.on("end", () => {
-            console.info('Client disconnected.')
+        const incResult = await publisher.incr(CONNECTION_COUNT_KEY);
+        await publisher.publish(CONNECTION_COUND_UPDATED_CHANNEL, String(incResult));
+        io.on("end", async () => {
+            console.info('Client disconnected.');
+            const decrResult = await publisher.decr(CONNECTION_COUNT_KEY);
+            await publisher.publish(CONNECTION_COUND_UPDATED_CHANNEL, String(decrResult))
         })
     })
     app.get("/health-check", (_, reply) => {
