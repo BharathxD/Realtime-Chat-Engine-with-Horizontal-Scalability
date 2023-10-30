c02c9b1369dd344d21fb5102f942994f8628b161
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-10-30 17:59:16 +0530

    feat: initialize pub/sub using redisio

--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -1,4 +1,5 @@
 import { config } from 'dotenv';
+import Redis from 'ioredis';
 
 config();
 
@@ -6,15 +7,43 @@ const PORT: number = +(process.env.PORT || '3001');
 const HOST: string = process.env.HOST || '0.0.0.0';
 const CORS_ORIGIN: string = process.env.CORS_ORIGIN || 'http://localhost:3000';
 const REDIS_URL: string | undefined = process.env.REDIS_URL;
+const CONNECTION_COUNT_CHANNEL = "chat:connection-count";
+
 
 if (!REDIS_URL) {
     console.error('Missing REDIS_URL');
     process.exit(1);
 }
 
+/**
+ * A redis publisher
+ * ----
+ * Set it `rejectUnauthorized` to `true`, if you want to encrypt the data
+ * const publisher = new Redis(REDIS_URL, {
+ *  tls: {
+ *    rejectUnauthorized: true
+ *  }
+ * }
+ */
+const publisher = new Redis(REDIS_URL);
+
+/**
+ * A redis subscriber
+ * ----
+ * Set it `rejectUnauthorized` to `true`, if you want to encrypt the data
+ * const subscriber = new Redis(REDIS_URL, {
+ *  tls: {
+ *    rejectUnauthorized: true
+ *  }
+ * }
+ */
+const subscriber = new Redis(REDIS_URL);
+
 export {
     PORT,
     HOST,
     CORS_ORIGIN,
-    REDIS_URL
+    REDIS_URL,
+    publisher,
+    subscriber
 };
