61112797c901e2f94f7eb0abcc9a45c670b8b8e4
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-10-31 17:25:40 +0530

    feat: configure custom prettier config

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,12 +1,10 @@
-import { PORT } from './config';
-import startServer from './utils/startServer';
 import createFastifyApp from './utils/createFastifyApp';
 
 /**
  * Main function to start the Fastify server and define a health-check route.
  */
 const main = async () => {
-    const app = await createFastifyApp();
+    await createFastifyApp();
 };
 
 main(); 
