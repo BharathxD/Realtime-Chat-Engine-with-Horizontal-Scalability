4d9e2432f0aacda8e3ee0817a1ed5cbde3aaa070
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-10-31 14:58:04 +0530

    feat: create message channel and improve documentation

--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -1,14 +1,58 @@
 import { config } from 'dotenv';
 
+/**
+ * Load environment variables from a .env file if present.
+ */
 config();
 
+/**
+ * The port on which the server should listen.
+ * Default is 3001 if not specified in the environment.
+ * @type {number}
+ */
 const PORT: number = +(process.env.PORT || '3001');
+
+/**
+ * The host on which the server should bind.
+ * Default is '0.0.0.0' if not specified in the environment.
+ * @type {string}
+ */
 const HOST: string = process.env.HOST || '0.0.0.0';
+
+/**
+ * The allowed origin for CORS (Cross-Origin Resource Sharing).
+ * Default is 'http://localhost:3000' if not specified in the environment.
+ * @type {string}
+ */
 const CORS_ORIGIN: string = process.env.CORS_ORIGIN || 'http://localhost:3000';
 
+/**
+ * The URL for the Redis server.
+ * @type {string | undefined}
+ */
 const REDIS_URL: string | undefined = process.env.REDIS_URL;
-const CONNECTION_COUNT_KEY = "chat:connection-count";
-const CONNECTION_COUNT_UPDATED_CHANNEL = "chat:connection-count-updated";
+
+/**
+ * An object to store Redis-related channel names.
+ */
+const RedisChannels = {
+    /**
+     * Key for storing the count of connections in Redis.
+     */
+    CONNECTION_COUNT_KEY: "chat:connection-count",
+    /**
+     * Channel for notifying updates to the connection count in Redis.
+     */
+    CONNECTION_COUNT_UPDATED_CHANNEL: "chat:connection-count-updated",
+    /**
+     * Channel for notifying new messages in the chat.
+     */
+    NEW_MESSAGE_CHANNEL: "chat:new-message",
+    /**
+     * Key for storing chat messages in Redis.
+     */
+    MESSAGES_KEY: "chat:messages",
+} as const;
 
 if (!REDIS_URL) {
     console.error('Missing REDIS_URL');
@@ -20,6 +64,5 @@ export {
     HOST,
     CORS_ORIGIN,
     REDIS_URL,
-    CONNECTION_COUNT_KEY,
-    CONNECTION_COUNT_UPDATED_CHANNEL
+    RedisChannels,
 };
--- api/src/utils/createFastifyApp.ts
+++ api/src/utils/createFastifyApp.ts
@@ -2,12 +2,19 @@ import fastify, { FastifyInstance } from "fastify";
 import fastifyCors from '@fastify/cors';
 import fastifyIO from "fastify-socket.io";
 
-import { CONNECTION_COUNT_UPDATED_CHANNEL, CONNECTION_COUNT_KEY, CORS_ORIGIN, PORT } from '../config';
+import { RedisChannels, CORS_ORIGIN, PORT } from '../config';
 import { publisher, subscriber } from "../config/redis";
 import startServer from "./startServer";
 import closeWithGrace from "close-with-grace";
+import { randomUUID } from "crypto";
 
-let connectedClients = 0;
+/**
+ * Why are we maintaining the `connectedClients` variable here when there is an implementation to decrement the client count on io disconnection?
+ * Well, when we save the code, the server basically restarts itself to show the changes. In that scenario, the io disconnection logic will never be
+ * hit, and hence the client count stays ambiguous. The `connectedClients` variable will set the connectedClients value to `zero` every time the server
+ * restarts, and we have also implemented the graceful shutdown at the end. So, even the Connected Clients KV pair in the redis will be reset.
+ */
+let connectedClients: number = 0;
 
 /**
  * Creates and configures the Fastify app with real-time communication capabilities.
@@ -24,9 +31,9 @@ const createFastifyApp = async (): Promise<FastifyInstance> => {
     await app.register(fastifyIO);
 
     // Initialize the connection count in Redis if not already set.
-    let currentCount = await publisher.get(CONNECTION_COUNT_KEY);
+    let currentCount = await publisher.get(RedisChannels.CONNECTION_COUNT_KEY);
     if (!currentCount) {
-        await publisher.set(CONNECTION_COUNT_KEY, 0);
+        await publisher.set(RedisChannels.CONNECTION_COUNT_KEY, 0);
     }
 
     // Handle WebSocket connections and disconnections.
@@ -34,35 +41,57 @@ const createFastifyApp = async (): Promise<FastifyInstance> => {
         console.info("Client connected.");
 
         // Increase the connection count and notify clients.
-        const incResult = await publisher.incr(CONNECTION_COUNT_KEY);
-        await publisher.publish(CONNECTION_COUNT_UPDATED_CHANNEL, String(incResult));
+        const incResult = await publisher.incr(RedisChannels.CONNECTION_COUNT_KEY);
+        await publisher.publish(RedisChannels.CONNECTION_COUNT_UPDATED_CHANNEL, String(incResult));
         ++connectedClients;
+
+        io.on(RedisChannels.NEW_MESSAGE_CHANNEL, async ({ message }) => {
+            return await publisher.publish(RedisChannels.NEW_MESSAGE_CHANNEL, message.toString())
+        })
 
         // Handle client disconnections.
         io.on("disconnect", async () => {
             console.info('Client disconnected.');
 
             // Decrease the connection count and notify clients.
-            const decrResult = await publisher.decr(CONNECTION_COUNT_KEY);
-            await publisher.publish(CONNECTION_COUNT_UPDATED_CHANNEL, String(decrResult));
+            const decrResult = await publisher.decr(RedisChannels.CONNECTION_COUNT_KEY);
+            await publisher.publish(RedisChannels.CONNECTION_COUNT_UPDATED_CHANNEL, String(decrResult));
             --connectedClients;
         })
     })
 
     // Subscribe to the connection count update channel in Redis.
-    subscriber.subscribe(CONNECTION_COUNT_UPDATED_CHANNEL, (err, count) => {
-        if (err) return console.error(`Error subscribing to ${CONNECTION_COUNT_UPDATED_CHANNEL}`, err);
+    subscriber.subscribe(RedisChannels.CONNECTION_COUNT_UPDATED_CHANNEL, (err, count) => {
+        if (err) {
+            return console.error(`Error subscribing to ${RedisChannels.CONNECTION_COUNT_UPDATED_CHANNEL}`, err)
+        };
 
-        console.log(`${count} ${count !== 1 ? "clients" : "client"} is connected to ${CONNECTION_COUNT_UPDATED_CHANNEL}`);
+        console.log(`${count} ${count !== 1 ? "clients" : "client"} is subscribed to ${RedisChannels.CONNECTION_COUNT_UPDATED_CHANNEL}`);
+    })
+
+    subscriber.subscribe(RedisChannels.NEW_MESSAGE_CHANNEL, (err, count) => {
+        if (err) {
+            return console.error(`Error subscribing to ${RedisChannels.NEW_MESSAGE_CHANNEL}`);
+        }
+
+        console.log(`${count} ${count !== 1 ? "clients" : "client"} is subscribed to ${RedisChannels.NEW_MESSAGE_CHANNEL}`);
     })
 
     // Handle messages received on the connection count update channel.
     subscriber.on('message', (channel, text) => {
-        if (channel === CONNECTION_COUNT_UPDATED_CHANNEL) {
+        if (channel === RedisChannels.CONNECTION_COUNT_UPDATED_CHANNEL) {
             // Broadcast the updated connection count to all connected clients.
-            app.io.emit(CONNECTION_COUNT_UPDATED_CHANNEL, {
+            return app.io.emit(RedisChannels.CONNECTION_COUNT_UPDATED_CHANNEL, {
                 count: text
             });
+        }
+        if (channel === RedisChannels.NEW_MESSAGE_CHANNEL) {
+            return app.io.emit(RedisChannels.NEW_MESSAGE_CHANNEL, {
+                id: randomUUID(),
+                message: text,
+                createdAt: new Date(),
+                port: PORT
+            })
         }
     })
 
@@ -76,9 +105,9 @@ const createFastifyApp = async (): Promise<FastifyInstance> => {
     closeWithGrace({ delay: 2000 }, async () => {
         if (connectedClients > 0) {
             console.log(`Removing ${connectedClients} from the count`);
-            const currentCount = +(await publisher.get(CONNECTION_COUNT_KEY) || '0')
+            const currentCount = +(await publisher.get(RedisChannels.CONNECTION_COUNT_KEY) || '0')
             const newCount = Math.max(currentCount - connectedClients, 0);
-            await publisher.set(CONNECTION_COUNT_KEY, newCount)
+            await publisher.set(RedisChannels.CONNECTION_COUNT_KEY, newCount)
         }
         await app.close();
     });
